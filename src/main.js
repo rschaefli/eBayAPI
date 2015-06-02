5c447ee210e218b7eeed5839a807f1812d9c083a
rschaefli <rschaefli@natera.com>
2015-06-02 14:10:02 -0700

    Use node-scheduler

--- src/main.js
+++ src/main.js
@@ -1,26 +1,43 @@
 var http = require("http");
+var schedule = require('node-schedule');
 
 var apiKey = "RicardoS-0c4e-433c-9c47-b233462c797d";
 var query = "pokemon%20base%20set%20first%20edition%20booster%20box";
 
-http.get("http://svcs.ebay.com/services/search/FindingService/v1?" +
-          "OPERATION-NAME=findItemsByKeywords" +
-          "&SERVICE-VERSION=1.0.0" +
-          "&SECURITY-APPNAME=" + apiKey +
-          "&RESPONSE-DATA-FORMAT=JSON" +
-          "&REST-PAYLOAD" +
-          "&keywords=" + query, function(res) {
+var cronPattern = "* * * * *";
 
-  console.log("Got response: " + res.statusCode);
-  var body = '';
-  res.on('data', function(chunk) {
-    body += chunk;
+
+var makeApiQuery = function(cb) {
+  http.get("http://svcs.ebay.com/services/search/FindingService/v1?" +
+            "OPERATION-NAME=findItemsByKeywords" +
+            "&SERVICE-VERSION=1.0.0" +
+            "&SECURITY-APPNAME=" + apiKey +
+            "&RESPONSE-DATA-FORMAT=JSON" +
+            "&REST-PAYLOAD" +
+            "&keywords=" + query, function(res) {
+
+    var body = '';
+
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
+
+    res.on('end', function () {
+      var result = JSON.parse(body);
+      cb(result);
+    });
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+    cb(undefined);
   });
-  res.on('end', function () {
-    var result = JSON.parse(body);
-    console.log(result.findItemsByKeywordsResponse[0].searchResult[0].item[0]);
-    console.log(result.findItemsByKeywordsResponse[0].paginationOutput);
-  });
-}).on('error', function(e) {
-  console.log("Got error: " + e.message);
+}
+
+
+schedule.scheduleJob(cronPattern, function() {
+    console.log('Cron job running..');
+
+    makeApiQuery(function callback(result) {
+      console.log(result.findItemsByKeywordsResponse[0].searchResult[0].item[0]);
+      console.log(result.findItemsByKeywordsResponse[0].paginationOutput);
+    });
 });
